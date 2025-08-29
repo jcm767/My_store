8c729994878324ccfc97dda4bcbac833c9b4eeac
jcm767 <jcm765@yahoo.com>
2025-08-29 16:13:14 -0400

    Create supabaseAdmin.ts

---
+++ app/lib/supabaseAdmin.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const serviceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
+
+const supabaseAdmin = createClient(url, serviceKey, { auth: { persistSession: false } })
+export default supabaseAdmin
